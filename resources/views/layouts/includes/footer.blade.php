bf5db59ff073bf2c82395af88101de49db26d0f7
zinhoflag1 <zinhoflag1@gmail.com>
2023-12-13 19:00:41 -0300

    13.12.202319.00

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,11 +1,8 @@
 <div class="row justify-content-center print" id="footer">
-    <div class="col"></div>
     <div class="col-md-auto p-3">
         <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.2.0
-        </div>
-        <strong>Copyright &copy; 2012-2023 <a href="https://defesacivil.mg.gov.br">Gabinete Militar do Governador e Coordenadoria Estadual de Defesa Civil de Minas Gerais</a>.</strong> Todos os
+            Version 
+        <strong style='font-size:10pt'>Copyright &copy; 2012-2023 <a href="https://defesacivil.mg.gov.br">Gabinete Militar do Governador e Coordenadoria Estadual de Defesa Civil de Minas Gerais</a>.</strong> Todos ospt
         direitos reservados.
     </div>
-    <div class="col"></div>
 </div>
